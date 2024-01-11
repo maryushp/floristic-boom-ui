e54a0fbfffc3c0d2ad7ae068bb75bcb09f6d2040
vburmus <burmusvolod@gmail.com>
2024-01-11 13:58:47 +0100

    feat(config) added bootstrap and refactored styles to sass

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {BrowserRouter as Router} from "react-router-dom";
-import './styles/sass/index.css';
+import './styles/css/index.css';
 import App from './App';
 import {createRoot} from "react-dom/client";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 const root = document.getElementById('root');
 if (root) {
     const rootReact = createRoot(root);
