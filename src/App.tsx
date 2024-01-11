d58c8eb996fae08a0c96ceebcc57d340c6a6f376
vburmus <burmusvolod@gmail.com>
2024-01-11 19:16:26 +0100

    fix(home) structure of App and index style

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
+import Home from "./pages/Home";
+import {Route, Routes} from "react-router-dom";
 
 
 function App() {
     return (
-        <main>
+        <>
             <Header/>
+            <main>
+                <Routes>
+                    <Route path={"/"} element={<Home/>}/>
+                </Routes>
+            </main>
             <Footer/>
-        </main>
+        </>
     )
 }
 
