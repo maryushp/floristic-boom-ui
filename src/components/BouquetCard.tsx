e3606b4e4730cb93405a708359fec91d8fa587c9
vburmus <burmusvolod@gmail.com>
2024-01-12 22:17:44 +0100

    fix(flower/bouquet) added link to details

--- src/components/BouquetCard.tsx
+++ src/components/BouquetCard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Bouquet, Flower} from "../types";
 import {Image} from "react-bootstrap";
 import {Cart} from "react-bootstrap-icons";
+import {Link} from "react-router-dom";
 
 type BouquetCardProps = {
     bouquet: Bouquet
@@ -10,15 +11,17 @@ type BouquetCardProps = {
 const BouquetCard = (props: BouquetCardProps) => {
     const {bouquet} = props
     return (
-        <div className="card card-bouquet" style={{width:300}}>
+        <div className="card card-bouquet" style={{width: 300}}>
             <Image src={bouquet.imageUri} className="card-img-top"/>
             <div className="card-body">
-                <h5 className="card-title fw-bold">{bouquet.name}</h5>
+                <Link to={"/flower/" + bouquet.id} className="text-black">
+                    <h5 className="card-title fw-bold">{bouquet.name}</h5>
+                </Link>
                 <p className="card-text">{bouquet.description}</p>
-            <p className="card-text">
+                <p className="card-text">
                     Flowers: {bouquet.flowers.map((flower: Flower) => {
                     return flower.name
-                }).join(", ") }</p>
+                }).join(", ")}</p>
                 <div className="d-flex justify-content-between align-items-center">
                     <h4 className="card-text pt-2 ">{bouquet.price}zł</h4>
                     <Cart size={24} color="green"/>
--- src/components/FlowerCard.tsx
+++ src/components/FlowerCard.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import {Bouquet, Flower} from "../types";
+import {Flower} from "../types";
 import {Image} from "react-bootstrap";
-import {Cart, PlusCircle} from "react-bootstrap-icons";
-
+import {PlusCircle} from "react-bootstrap-icons";
+import {Link} from "react-router-dom";
+import "../styles/css/Card.css"
 type FlowerCardProps = {
     flower: Flower
 }
@@ -10,17 +11,21 @@ type FlowerCardProps = {
 const FlowerCard = (props: FlowerCardProps) => {
     const {flower} = props
     return (
-        <div className="card card-flower" style={{width:300}}>
-            <Image src={flower.imageUri} className="card-img-top"/>
-            <div className="card-body">
-                <h5 className="card-title fw-bold">{flower.name}</h5>
-                <p className="card-text">{flower.description}</p>
+        <div className="card card-flower shadow" style={{width: 300}}>
 
+            <Image src={flower.imageUri} className="card-img-top"/>
+
+            <div className="card-body">
+                <Link to={"/flower/" + flower.id} className="text-black">
+                    <h5 className="card-title fw-bold">{flower.name}</h5>
+                    <p className="card-text">{flower.description}</p>
+                </Link>
                 <div className="d-flex justify-content-between align-items-center">
                     <h4 className="card-text pt-2 ">{flower.price}zł</h4>
                     <PlusCircle size={24} color="green"/>
                 </div>
             </div>
+
         </div>)
 };
 
