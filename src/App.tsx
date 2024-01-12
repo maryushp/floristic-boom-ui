9cdd6632e5495167c4b082ec539fe4c1776e0848
maryushp <maryushp@gmail.com>
2024-01-12 20:17:30 +0100

    feat(routing) provided new routing paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
-
+import {Route, Routes} from "react-router-dom";
+import CartPage from "./pages/CartPage";
+import CreateOrderPage from "./pages/CreateOrderPage";
 
 function App() {
-    return (
-        <main>
-            <Header/>
-            <Footer/>
-        </main>
-    )
+  return (
+      <main>
+          <Header/>
+          <Routes>
+              <Route element={<CartPage/>} path={"/cart"}/>
+              <Route element={<CreateOrderPage/>} path={"/create-order"}/>
+          </Routes>
+          <Footer/>
+      </main>
+  );
 }
 
 export default App;
