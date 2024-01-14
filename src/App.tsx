de09e62f1625e3e7ff13438e57b7e221b492dc8a
vburmus <burmusvolod@gmail.com>
2024-01-15 00:28:58 +0100

    feat(routes) updated routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,28 @@ import React from "react";
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
 import {Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
 import CartPage from "./pages/CartPage";
 import CreateOrderPage from "./pages/CreateOrderPage";
-import Home from "./pages/Home";
+import BouquetsPage from "./pages/BouquetsPage";
+import FlowersPage from "./pages/FlowersPage";
+
 
 function App() {
     return (
-        <main>
+        <>
             <Header/>
             <main>
                 <Routes>
                     <Route path={"/"} element={<Home/>}/>
                     <Route element={<CartPage/>} path={"/cart"}/>
                     <Route element={<CreateOrderPage/>} path={"/create-order"}/>
+                    <Route path="/bouquets" element={<BouquetsPage/>}></Route>
+                    <Route path="/flowers" element={<FlowersPage/>}></Route>
                 </Routes>
             </main>
             <Footer/>
-        </main>
+        </>
     );
 }
 
