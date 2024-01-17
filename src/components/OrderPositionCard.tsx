bc582121d01b5638d7219c9618de36fcae9b57c0
maryushp <maryushp@gmail.com>
2024-01-17 19:22:47 +0100

    fix(order) fixed order position card style

--- src/components/OrderPositionCard.tsx
+++ src/components/OrderPositionCard.tsx
@@ -13,7 +13,7 @@ const OrderPositionCard = (props: OrderPositionProps) => {
 
     return (
         <Link to={`/flower/${bouquet.id}`}>
-            <div className="border border-success border-2 d-flex flex-column align-items-center gap-2 rounded-4 p-3">
+            <div className="border border-success border-2 d-flex flex-column align-items-center gap-2 rounded-4 p-4">
                 <Image src={bouquet.imageUri} className="position-img"/>
                 <h4 className="text-black text-center fw-bold text-capitalize">{bouquet.name}</h4>
                 <h5 className="text-black text-center fw-bold">Amount: {quantity}</h5>
