ac0443bd65a0c9f75478967b65829dd54e801e17
vburmus <burmusvolod@gmail.com>
2024-01-17 19:28:16 +0100

    fix(custom-bouquet) added func to read custom bouquets

--- src/utils/bouquetUtils.ts
+++ src/utils/bouquetUtils.ts
@@ -1,9 +1,25 @@
 import axiosInstance from "./axiosConfig";
+import {BouquetCreationRequest} from "./types";
+import {getUserFromStorage} from "./userUtils";
+
+const bouquetsEndpoint = "/api/v1/bouquet";
 
 const readAllBouquets = async (currentPage: number, currentSize: number) => {
-    const bouquetsEndpoint = "/api/v1/bouquet";
     const pageParams = `?page=${currentPage - 1}&size=${currentSize}`;
     const response = await axiosInstance.get(bouquetsEndpoint + pageParams)
     return response.data
 }
-export {readAllBouquets}
+
+const readUserBouquets = async (currentPage: number, currentSize: number) => {
+    const pageParams = `?page=${currentPage - 1}&size=${currentSize}`;
+    const user = getUserFromStorage()
+
+    const response = await axiosInstance.get(bouquetsEndpoint + "/by-user/" + user?.id + pageParams)
+    return response.data
+}
+
+const createBouquet = async (bcr: BouquetCreationRequest) => {
+    const response = await axiosInstance.post(bouquetsEndpoint, bcr)
+    return response.data
+}
+export {readAllBouquets, createBouquet, readUserBouquets}
